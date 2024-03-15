66235eb745004198bbd3a864698de3722331325d
Michael Pollind <mpollind@gmail.com>
2024-03-15 15:57:51 -0700

    omit spamming log with memory dump
    Signed-off-by: Michael Pollind <mpollind@gmail.com>

--- source/qcommon/common.c
+++ source/qcommon/common.c
@@ -1046,7 +1046,7 @@ void Qcommon_Shutdown( void )
 	Qcommon_ShutdownCommands();
 	Memory_ShutdownCommands();
 
-	Mem_DumpMemoryReport();
+	//Mem_DumpMemoryReport();
 	Com_CloseConsoleLog( true, true );
 
    Qcommon_ShutdownCvarDescriptions();
