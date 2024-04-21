f5b40fe9b8391e57a49c3ad428d89e5f3432f781
CoolElectronics <me@coolelectronics.me>
2024-04-21 12:03:08 -0400

    fix: calculate scoreboard offsets relative to font size

--- source/cgame/cg_scoreboard.cpp
+++ source/cgame/cg_scoreboard.cpp
@@ -571,8 +571,9 @@ static int SCR_DrawTeamTab( const char **ptrptr, int *curteam, int x, int y, int
 			continue;
 
 		if (strcmp(token,"AVATAR") == 0){
-			// special case, don't draw any text, just skip ahead 32px
-			xoffset += 32;
+			// special case, don't draw any text, just skip ahead by avatarsize
+			int avatarsize = trap_SCR_FontHeight( font ) * 1.25;
+			xoffset += avatarsize;
 			continue;
 		}
 
@@ -682,9 +683,12 @@ static int SCR_DrawPlayerTab( const char **ptrptr, int team, int x, int y, int p
 	xoffset = 0;
 	yoffset = 0;
 
-	height = 32;
 
-	int textoffset = (32-trap_SCR_FontHeight( font ))/2;
+	int avatarsize = trap_SCR_FontHeight( font )*1.25;
+
+	height = avatarsize;
+
+	int textoffset = (avatarsize-trap_SCR_FontHeight( font ))/2;
 
 	// start from the center again
 	xoffset = CG_HorizontalAlignForWidth( 0, align, panelWidth );
@@ -804,7 +808,7 @@ static int SCR_DrawPlayerTab( const char **ptrptr, int team, int x, int y, int p
 				}
 				if (i >= 0 && i < gs.maxclients && cgs.clientInfo[i].steamid)
 					avatar = cgs.clientInfo[i].avatar;
-				width = 32;
+				width = avatarsize;
 			break;
 		}
 
@@ -834,7 +838,7 @@ static int SCR_DrawPlayerTab( const char **ptrptr, int team, int x, int y, int p
 				if (trans)
 					avColor[3] = 0.3;
 
-				trap_R_DrawStretchPic(x+xoffset,y+yoffset,32,32,0,0,1,1,avColor,avatar);
+				trap_R_DrawStretchPic(x+xoffset,y+yoffset,avatarsize,avatarsize,0,0,1,1,avColor,avatar);
 			}
 		}
 
@@ -852,12 +856,12 @@ static int SCR_DrawPlayerTab( const char **ptrptr, int team, int x, int y, int p
 		xoffset += width;
 	}
 
-	int gap = 6;
+	int gap = trap_SCR_FontHeight(font) / 4;
 	if (pass && !last){
 		// draw scoreboard separator
 		CG_TeamColor( team, teamcolor );
 		teamcolor[3] = SCB_BACKGROUND_ALPHA - 0.17;
-		trap_R_DrawStretchPic( xstart, y + yoffset +32,panelWidth+gap, gap, 0, 0, 1, 1,teamcolor, cgs.shaderWhite );
+		trap_R_DrawStretchPic( xstart, y + yoffset + avatarsize,panelWidth+gap, gap, 0, 0, 1, 1,teamcolor, cgs.shaderWhite );
 	}
 
 	height += gap;
