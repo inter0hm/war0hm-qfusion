9630954afc32b4cdf5c80bc411ce6320a11194a4
CoolElectronics <me@coolelectronics.me>
2024-04-24 17:07:14 -0400

    steamshim: send heartbeats on separate thread

--- source/steamshim/src/parent/parent.cpp
+++ source/steamshim/src/parent/parent.cpp
@@ -21,6 +21,7 @@ freely, subject to the following restrictions:
 #include <cstdint>
 #include <cstring>
 #include <stdio.h>
+#include <thread>
 #define DEBUGPIPE 1
 #include "parent.h"
 #include "../steamshim.h"
@@ -136,6 +137,13 @@ static bool setEnvironmentVars(PipeType pipeChildRead, PipeType pipeChildWrite)
     return true;
 } 
 
+static void taskHeartbeat(){
+    while (true) {
+        Write1ByteMessage(SHIMCMD_PUMP);
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    }
+}
+
 extern "C" {
   int STEAMSHIM_init(SteamshimOptions *options)
   {
@@ -190,6 +198,10 @@ extern "C" {
     closePipe(pipeChildWrite);
 
     pipeChildRead = pipeChildWrite = NULLPIPE;
+
+
+    std::thread task(taskHeartbeat);
+    task.detach();
 
 #ifndef _WIN32
       signal(SIGPIPE, SIG_IGN);
